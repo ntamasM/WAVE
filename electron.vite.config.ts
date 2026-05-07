cd44edc8dde64dcc3846644ae3b05dcfc62e0b10
Ntamas <manolis.ntamadakis@gmail.com>
2026-05-07 11:08:52 +0300

    Refactor preload API and update renderer configuration
    - Refactored the preload API in index.js to streamline the code and add new methods for update management.
    - Updated index.html to reference new asset files and added module preloading for improved performance.
    - Created electron.vite.config.ts to configure Vite for building the Electron app, including main, preload, and renderer processes with optimized chunking and minification.

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,14 +1,14 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'electron-vite';
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import { resolve } from 'path';
 
 export default defineConfig({
   main: {
+    plugins: [externalizeDepsPlugin()],
     build: {
       outDir: 'out/main',
       minify: true,
       rollupOptions: {
-        external: ['velopack'],
         input: {
           index: resolve(__dirname, 'src/main/index.ts'),
         },
@@ -19,11 +19,11 @@ export default defineConfig({
     },
   },
   preload: {
+    plugins: [externalizeDepsPlugin()],
     build: {
       outDir: 'out/preload',
       minify: true,
       rollupOptions: {
-        external: ['velopack'],
         input: {
           index: resolve(__dirname, 'src/preload/index.ts'),
         },
